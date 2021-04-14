aa39ebf013e9e0e9a9816221a3031175bccdb126
hhi9920 <hhi9920@naver.com>
2021-04-15 00:47:46 +0900

    first spring boot project

---
+++ src/test/java/kr/ac/hansung/cse/hellospringboot/HellospringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.ac.hansung.cse.hellospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellospringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
